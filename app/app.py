f92941d0207d9aaf8f43a90ec49c57058620c521
rialtoscuro <rialtoscuro@outlook.com>
2025-12-13 16:43:43 +0100

    End poit to get syntheticData

--- app/app.py
+++ app/app.py
@@ -5,14 +5,7 @@ _app = Flask(__name__, template_folder='templates', static_folder='static')
 
 @_app.route('/')
 def endpointHome():
-    """
-    Endpoint that loads the home HTML
-
-    :return: main web page
-    :rtype: HTML
-    """
     return render_template('index.html')
-
 
 @_app.route('/predict', methods=['POST'])
 def predict():
--- medulloblastoma/synthetic/synthetic.py
+++ medulloblastoma/synthetic/synthetic.py
@@ -1,13 +1,16 @@
 import torch
 
-from src.globals import GLOBAL_NUM_INPUT_RANDOM_GENERATOR, GLOBAL_NUM_OUTPUT_SYNTHETIC
-from src.model.mGenerator import mGeneratorV1_2886  # same class as used for training
+from globals import GLOBAL_NUM_INPUT_RANDOM_GENERATOR, GLOBAL_NUM_OUTPUT_SYNTHETIC
+from model.mGenerator import *  # same class as used for training
 
 
-device = "cuda" if torch.cuda.is_available() else "cpu"
-latent_dim = GLOBAL_NUM_INPUT_RANDOM_GENERATOR
-G = mGeneratorV1_2886().to(device)
-G.load_state_dict(torch.load("mGeneratorV1_2886_1430.pt", map_location=device))
-G.eval()
-with torch.no_grad():
-    synthetic_data = G(torch.randn(GLOBAL_NUM_OUTPUT_SYNTHETIC, latent_dim).to(device))
+
+
+def getSyntheticData(modelObj, modelSrc):
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    latent_dim = GLOBAL_NUM_INPUT_RANDOM_GENERATOR
+    G = modelObj.to(device)
+    G.load_state_dict(torch.load(modelSrc, map_location=device))
+    G.eval()
+    with torch.no_grad():
+        return G(torch.randn(GLOBAL_NUM_OUTPUT_SYNTHETIC, latent_dim).to(device))
